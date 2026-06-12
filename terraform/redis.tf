c686dd4dbe9162513dbf6faba0df3d3235a1ffd2
ZeusOfDev <hinhnhula1234567@gmail.com>
2026-06-12 23:40:51 +0700

    add scheme redis tls

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,17 +1,17 @@
 resource "azurerm_redis_cache" "metrics" {
-  name                 = "${var.project_name}-redis-metrics-${var.environment}"
-  location             = data.azurerm_resource_group.main.location
-  resource_group_name  = data.azurerm_resource_group.main.name
+  name                = "${var.project_name}-redis-metrics-${var.environment}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
 
-  capacity             = 0          # C0 = 250MB, quá đủ cho 1 app nhỏ
+  capacity             = 0 # C0 = 250MB, quá đủ cho 1 app nhỏ
   family               = "C"
   sku_name             = "Standard" # giữ Standard vì production cần SLA 99.9%
   minimum_tls_version  = "1.2"
-  non_ssl_port_enabled = false
+  non_ssl_port_enabled = true
 
   redis_configuration {
     maxmemory_policy                = "noeviction"
-    maxmemory_reserved              = 30  # giảm xuống vì capacity nhỏ hơn
+    maxmemory_reserved              = 30 # giảm xuống vì capacity nhỏ hơn
     maxfragmentationmemory_reserved = 30
   }
 
